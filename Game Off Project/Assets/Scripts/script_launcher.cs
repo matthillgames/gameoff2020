2578965c67e3fea6a55415de91629b54309602ff
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-08 11:47:49 -0800

    Folders and first script
    Creates work folders for assets, and started work on the launcher script

---
+++ Game Off Project/Assets/Scripts/script_launcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems
+
+public class script_launcher : MonoBehaviour, IPointerDownHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Click Detected");
+    }
+}
