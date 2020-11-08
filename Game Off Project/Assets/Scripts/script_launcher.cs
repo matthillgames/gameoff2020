78246a2a544404f28af8513443334832f63ec807
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-08 12:42:18 -0800

    Started Click & Drag Fuction
    Created functions for detecting mouse click and release, and function to draw line to mouse.

--- Game Off Project/Assets/Scripts/script_launcher.cs
+++ Game Off Project/Assets/Scripts/script_launcher.cs
@@ -1,24 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems
+﻿using UnityEngine;
 
-public class script_launcher : MonoBehaviour, IPointerDownHandler
+public class script_launcher : MonoBehaviour
 {
+    public GameObject line;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Game Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (line.activeSelf == true)
+        {
+            drawLine();
+        }
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnMouseDown()
     {
-        Debug.Log("Mouse Click Detected");
+        Debug.Log("Click");
+        line.SetActive(true);
     }
+
+    public void OnMouseUp()
+    {
+        Debug.Log("UnClick");
+        line.SetActive(false);
+    }
+
+    void drawLine() //Draws a line from this object to the mouse pointer
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = transform.position.z;
+        
+        line.GetComponent<LineRenderer>().SetPosition(1, mousePos);
+    }
+
 }
