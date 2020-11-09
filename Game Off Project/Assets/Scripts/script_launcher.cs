8faa0b5ebed10092a14f8cfc164282ee2371e11e
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-09 00:40:52 -0800

    Finished Launcher
    Finished building the script for launching moons.

--- Game Off Project/Assets/Scripts/script_launcher.cs
+++ Game Off Project/Assets/Scripts/script_launcher.cs
@@ -5,37 +5,59 @@ public class script_launcher : MonoBehaviour
     public GameObject line;
     public GameObject projectile;
     public float LINE_MAX_LENGTH = 3f;
-    public float MAX_POWER = 10f;
+    public float MAX_POWER = 1000f;
+    float COOLDOWN = 0.1f;
     float power = 0f;
+    float timer = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Game Start");
-    }
 
     // Update is called once per frame
     void Update()
     {
+        //Check if we need to draw line
         if (line.activeSelf == true)
         {
             drawLine();
         }
+        //Check cooldown timer
+        else if (timer > 0) 
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                GetComponent<SphereCollider>().enabled = true;
+            }
+        }
     }
+
+
 
     public void OnMouseDown()
     {
         Debug.Log("Click");
         line.SetActive(true);
+
+        //The physics require we turn off collision while the projectile is spawned
+        //A timer is set to turn it back on
+        GetComponent<SphereCollider>().enabled = false;
+        timer = COOLDOWN;
     }
 
     public void OnMouseUp()
     {
         Debug.Log("UnClick");
         line.SetActive(false);
+
+        //Create a Moon Object and give it a force in the direction of the mouse
+        var p = Instantiate(projectile);
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = transform.position.z;
+        p.transform.LookAt(mousePos);
+        p.GetComponent<Rigidbody>().AddForce(p.transform.forward * MAX_POWER * power,ForceMode.Impulse);
+        Debug.Log(MAX_POWER * power * Time.deltaTime);
     }
 
-    void drawLine() //Draws a line from this object to the mouse pointer
+    void drawLine() //Draws a line from this object to the mouse pointer, obeying max length
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = transform.position.z;
