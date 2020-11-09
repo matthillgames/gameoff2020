acc7d07801c2761dc12fe8ccb6a29c0ec3e5bbb3
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-09 01:28:01 -0800

    Added Planets
    Added Planet Prefab that attracts moons within its trigger

---
+++ Game Off Project/Assets/Scripts/script_gravity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class script_gravity : MonoBehaviour
+{
+    public float GRAVITY = 5000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.LookAt(transform.position);
+        other.attachedRigidbody.AddForce(other.transform.forward * GRAVITY * Time.deltaTime / Mathf.Pow(Vector3.Distance(other.transform.position, transform.position),2f), ForceMode.Force);
+    }
+}
