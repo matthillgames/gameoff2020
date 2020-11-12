4afa32a9a0378067d114a09c7c835a245c4899a1
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-11 23:42:18 -0800

    Fixed Gravity
    Forgot to make a branch for this, but oh well.

--- Game Off Project/Assets/Scripts/script_gravity.cs
+++ Game Off Project/Assets/Scripts/script_gravity.cs
@@ -3,6 +3,8 @@
 public class script_gravity : MonoBehaviour
 {
     public float GRAVITY = 5000f;
+    public float DAMPING_FORCE = 500f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,12 @@ public class script_gravity : MonoBehaviour
     {
         other.transform.LookAt(transform.position);
         other.attachedRigidbody.AddForce(other.transform.forward * GRAVITY * Time.deltaTime / Mathf.Pow(Vector3.Distance(other.transform.position, transform.position),2f), ForceMode.Force);
+
+        var localvel = other.transform.InverseTransformDirection(other.attachedRigidbody.velocity);
+        if (localvel.z < 0)
+        {
+            localvel.z *= .98f;
+            other.attachedRigidbody.velocity = other.transform.TransformDirection(localvel);
+        }
     }
 }
