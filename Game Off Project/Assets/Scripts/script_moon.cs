4b6ad3ca185d8713ec885c40705aa1cf465d175f
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-22 21:03:49 -0800

    Moons Destroy
    Moons destroy when they collide with eatch other or with a planet or asteroid.

---
+++ Game Off Project/Assets/Scripts/script_moon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_moon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Planet" || collision.gameObject.tag == "Moon")
+            Destroy(gameObject);
+
+    }
+}
