44363c7c3932b5fd1c4d70a948a6df7b88ce6882
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-23 01:34:34 -0800

    Made scene transition effect
    This took longer than the rest of the project so far...

---
+++ Game Off Project/Assets/Scripts/script_transition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class script_transition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    int levelIndex;
+    
+    public void changeLevel(int index)
+    {
+        levelIndex = index;
+        animator.SetTrigger("start");
+    }
+
+    public void Finish()
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
