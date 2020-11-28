9ebb851685c17e9bb17e5999103d321a8153020b
matthill92 <73971438+matthill92@users.noreply.github.com>
2020-11-27 20:42:04 -0800

    Moon effect on main menu

---
+++ Game Off Project/Assets/Scripts/script_menuLauncher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_menuLauncher : MonoBehaviour
+{
+    public Vector2 forceRange;
+    public Vector2 angleRange;
+
+    public float rate;
+
+    public GameObject moon;
+    float timer = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = rate;
+            var inst = Instantiate(moon, transform);
+            inst.transform.Rotate(new Vector3(0, 0, Random.Range(angleRange.x, angleRange.y)));
+            inst.GetComponent<Rigidbody>().AddForce(inst.transform.right * -forceRange);
+        }
+    }
+}
